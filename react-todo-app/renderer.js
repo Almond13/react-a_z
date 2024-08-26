f41c5bf40e74ad9bd640562fa60de5b6324edc08
seungyeon <ssyeon613@gmail.com>
2024-08-26 13:41:14 +0900

    electron & todoapp - 중복 알림 제거

--- react-todo-app/renderer.js
+++ react-todo-app/renderer.js
@@ -8,13 +8,3 @@ document.getElementById('toggle-dark-mode').addEventListener('click',
 document.getElementById('reset-to-system').addEventListener('click', async () => {
     await window.darkMode.system()
 })
-
-/* 알림 - 렌더 프로세스에서 실행 */
-const NOTIFICATION_TITLE = 'Electron app'
-const NOTIFICATION_BODY = 'Reload'
-// const CLICK_MESSAGE = 'Notification clicked!
-
-new window.Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY }).onclose = () => {
-    console.log('Notification closed');
-};
-//     .onclick = () => { document.getElementById('output').innerText = CLICK_MESSAGE }
